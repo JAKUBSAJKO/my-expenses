9d4aceabef3863f79ec37d1b4b67d4826bf8b762
JAKUBSAJKO <jakubsajko.inf@gmail.com>
2025-03-24 23:49:31 +0100

    Implement dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
-  return <div className="text-teal-500">My Expenses</div>;
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  const changeDarkMode = () => {
+    if (theme === "light") {
+      document.documentElement.classList.add("dark");
+      setTheme("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      setTheme("light");
+    }
+  };
+
+  return (
+    <div className="bg-white dark:bg-gray-900 text-teal-500">
+      My Expenses
+      <button onClick={changeDarkMode} className="ml-4 p-2 bg-teal-500 text-white rounded">
+        Enable Dark Mode
+      </button>
+    </div>
+  );
 }
