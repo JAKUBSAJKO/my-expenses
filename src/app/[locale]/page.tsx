ffa14eeb826afe8c6b9b62e9a9d8a155e0d8ec32
JAKUBSAJKO <jakubsajko.inf@gmail.com>
2025-03-30 21:21:30 +0200

    Add and setup translations package

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
 import { useState } from "react";
 
 export default function Home() {
   const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  const t = useTranslations("HomePage");
 
   const changeDarkMode = () => {
     if (theme === "light") {
@@ -21,6 +25,8 @@ export default function Home() {
       <button onClick={changeDarkMode} className="ml-4 p-2 bg-teal-500 text-white rounded">
         Enable Dark Mode
       </button>
+      <h1>{t("title")}</h1>
+      <Link href="/about">{t("about")}</Link>
     </div>
   );
 }
